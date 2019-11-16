c0067416f9ef206183675256c3954bb9a01b1862
DoctorOum <43484827+DoctorOum@users.noreply.github.com>
2019-11-15 20:49:52 -0500

    Created input manager for controller and keyboard keys

---
+++ JamVsJam2019/Assets/Scripts/Input Manager/TestInputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInputManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (InputManager.AButton())
+        {
+            Debug.Log(InputManager.MainJoystick());
+        }
+    }
+}
