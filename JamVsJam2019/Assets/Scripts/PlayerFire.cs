69e6eed74b0c3a563c45e401c0ab38aa42630104
Mint16384 <mint16384@gmail.com>
2019-11-16 13:20:54 -0500

    fire now has a reload functionality

--- JamVsJam2019/Assets/Scripts/PlayerFire.cs
+++ JamVsJam2019/Assets/Scripts/PlayerFire.cs
@@ -6,27 +6,53 @@ public class PlayerFire : MonoBehaviour
 {
     public GameObject bullet;
     bool isFire;
+    bool isReload;
     public float shootInterval;
+    public int bulletCount;
+    int bulletMax;
     int playerNumber;
 
     void Start()
     {
         isFire = false;
         playerNumber = GetComponent<PlayerMovement>().playerNumber;
+        bulletMax = bulletCount;
+        //StartCoroutine(PassiveReload());
     }
-    
+    void BulletIncrease(int count)
+    {
+        bulletCount += count;
+    }
     void Update()
     {
-        StartCoroutine(InputManager.BButton(playerNumber.ToString()) && !isFire ? Fire() : Empty());
+        StartCoroutine(InputManager.BButton(playerNumber.ToString()) && !isFire && bulletCount > 0 ? Fire() : Empty());
+        StartCoroutine(bulletCount <= 0 && !isReload ? Reload() : Empty());
     }
     IEnumerator Empty() { yield return new WaitForSeconds(0); }
+    IEnumerator Reload()
+    {
+        isFire = true;
+        isReload = true;
+
+        yield return new WaitForSeconds(3);
+        bulletCount = bulletMax;
+        isFire = false;
+        isReload = false;
+    }
+    IEnumerator PassiveReload()
+    {
+        BulletIncrease(bulletCount < bulletMax ? 1 : 0);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(PassiveReload());
+    }
     IEnumerator Fire()
     {
         isFire = true;
 
         GameObject test = Instantiate(bullet, transform.position, transform.rotation);
-        test.GetComponent<Rigidbody2D>().velocity = Vector2.up * 2 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
+        test.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
         test.tag = tag;
+        bulletCount -= 1;
 
         yield return new WaitForSeconds(shootInterval);
         isFire = false;
