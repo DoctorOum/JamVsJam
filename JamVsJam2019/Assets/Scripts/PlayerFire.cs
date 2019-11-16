fc5505b5d22b2b1f39db386f25374c6766a24f8e
Mint16384 <mint16384@gmail.com>
2019-11-15 20:50:41 -0500

    movement works

---
+++ JamVsJam2019/Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    public GameObject bullet;
+    bool isFire;
+    public float shootInterval;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    IEnumerator Fire()
+    {
+        isFire = true;
+
+
+
+        yield return new WaitForSeconds(shootInterval);
+        isFire = false;
+    }
+}
--- JamVsJam2019/Assets/Scripts/PlayerMovement.cs
+++ JamVsJam2019/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Range(0, 1)] int playerNumber;
+    [Range(1, 2)] public int playerNumber;
+    bool isMove;
 
     void Start()
     {
-        
+        isMove = false;
     }
     
     void Update()
     {
-        
+        if(playerNumber == 1 && !isMove)
+        {
+            if(Input.GetKey(KeyCode.A) && transform.position.x > 2)
+            {
+                StartCoroutine(UpdatePosition(Vector3.left));
+            }
+            if (Input.GetKey(KeyCode.D) && transform.position.x < 8)
+            {
+                StartCoroutine(UpdatePosition(Vector3.right));
+            }
+        }
+        else if(playerNumber == 2 && !isMove)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > 2)
+            {
+                StartCoroutine(UpdatePosition(Vector3.left));
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 8)
+            {
+                StartCoroutine(UpdatePosition(Vector3.right));
+            }
+        }
+    }
+    IEnumerator UpdatePosition(Vector3 addPosition)
+    {
+        isMove = true;
+
+        transform.position += addPosition;
+
+        yield return new WaitForSeconds(.125f);
+        isMove = false;
     }
 }
