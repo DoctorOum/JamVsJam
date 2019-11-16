85d4cc9b196f4a1dcef68efb386d12960cd006f3
Mint16384 <mint16384@gmail.com>
2019-11-15 19:58:35 -0500

    blah

---
+++ JamVsJam2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0, 1)] int playerNumber;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
