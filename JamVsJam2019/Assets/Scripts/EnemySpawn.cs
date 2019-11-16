7841fbde48df25f3ba33f05a484015b10e1593d8
Mint16384 <mint16384@gmail.com>
2019-11-16 15:38:04 -0500

    player2 can now spawn enemies

--- JamVsJam2019/Assets/Scripts/EnemySpawn.cs
+++ JamVsJam2019/Assets/Scripts/EnemySpawn.cs
@@ -25,19 +25,19 @@ public class EnemySpawn : MonoBehaviour
         spawningY = true;
 
         GameObject temp;
-        temp = Instantiate(Enemies[2], transform.position + new Vector3(-2f, 1.5f), transform.rotation);
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(-2f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1.5f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
         temp.tag = tag;
         temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
-        temp = Instantiate(Enemies[2], transform.position + new Vector3(-1f, 1f), transform.rotation);
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(-1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
         temp.tag = tag;
         temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
-        temp = Instantiate(Enemies[2], transform.position + new Vector3(0f, 1f), transform.rotation);
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(0f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
         temp.tag = tag;
         temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
-        temp = Instantiate(Enemies[2], transform.position + new Vector3(1f, 1f), transform.rotation);
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
         temp.tag = tag;
         temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
-        temp = Instantiate(Enemies[2], transform.position + new Vector3(2f, 1.5f), transform.rotation);
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(2f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1.5f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
         temp.tag = tag;
         temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
 
