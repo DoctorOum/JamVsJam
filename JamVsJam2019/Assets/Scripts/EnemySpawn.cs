cc0bbd8d7fd7d3d62dc30d9f121880d47ee1155c
Mint16384 <mint16384@gmail.com>
2019-11-16 15:57:14 -0500

    New spawn pattern

--- JamVsJam2019/Assets/Scripts/EnemySpawn.cs
+++ JamVsJam2019/Assets/Scripts/EnemySpawn.cs
@@ -7,6 +7,8 @@ public class EnemySpawn : MonoBehaviour
     public GameObject[] Enemies;
     int playerNumber;
     bool spawningY;
+    bool spawningA;
+
     void Start()
     {
         spawningY = false;
@@ -18,6 +20,10 @@ public class EnemySpawn : MonoBehaviour
         if(InputManager.YButton(playerNumber.ToString()) && !spawningY)
         {
             StartCoroutine(SpawnY());
+        }
+        if(InputManager.AButton(playerNumber.ToString()) && !spawningA)
+        {
+            StartCoroutine(SpawnA());
         }
     }
     IEnumerator SpawnY()
@@ -43,5 +49,29 @@ public class EnemySpawn : MonoBehaviour
 
         yield return new WaitForSeconds(8);
         spawningY = false;
+    }
+    IEnumerator SpawnA()
+    {
+        spawningA = true;
+
+        GameObject temp;
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(-1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1.5f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
+        temp.tag = tag;
+        temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(0f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
+        temp.tag = tag;
+        temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(0f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 3f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
+        temp.tag = tag;
+        temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(0f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 5f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
+        temp.tag = tag;
+        temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
+        temp = Instantiate(Enemies[2], transform.position + new Vector3(1f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI)), 1.5f * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI))), transform.rotation);
+        temp.tag = tag;
+        temp.GetComponent<Rigidbody2D>().velocity = Vector2.up * 8 * -Mathf.Round(Mathf.Cos(playerNumber * Mathf.PI));
+
+        yield return new WaitForSeconds(8);
+        spawningA = false;
     }
 }
