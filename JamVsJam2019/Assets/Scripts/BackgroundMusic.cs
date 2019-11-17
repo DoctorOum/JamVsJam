4d608109cf21b99d25b610572eea3ee1472e1de3
Sean <43484827+DoctorOum@users.noreply.github.com>
2019-11-16 20:19:06 -0500

    can quit from anywhere

--- JamVsJam2019/Assets/Scripts/BackgroundMusic.cs
+++ JamVsJam2019/Assets/Scripts/BackgroundMusic.cs
@@ -8,4 +8,11 @@ public class BackgroundMusic : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
     }
+    void Update()
+    {
+        if (Input.GetButtonDown("Quitty"))
+        {
+            Application.Quit();
+        }
+    }
 }
