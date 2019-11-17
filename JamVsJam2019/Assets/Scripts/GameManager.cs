d6b448b2d905ef4bbdfbe6e11ce92775761252f6
Mint16384 <mint16384@gmail.com>
2019-11-17 16:07:16 -0500

    gameplay works

--- JamVsJam2019/Assets/Scripts/GameManager.cs
+++ JamVsJam2019/Assets/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject Player1, Player2;
+    public UnityEngine.UI.Text timer;
+    public UnityEngine.UI.Image win1, win2, draw;
+    int timeClock = 5;
+    bool isTime = false;
 
     void Update()
     {
@@ -14,8 +18,39 @@ public class GameManager : MonoBehaviour
     IEnumerator Empty() { yield return new WaitForSeconds(0);  }
     IEnumerator PlayerWin(GameObject winner)
     {
-
-        yield return new WaitForSeconds(10);
-        SceneManager.LoadScene(0);
+        if (!isTime)
+        {
+            isTime = true;
+            timer.gameObject.SetActive(true);
+            StartCoroutine(Timer());
+            yield return new WaitForSeconds(10);
+            SceneManager.LoadScene(0);
+        }
+    }
+    IEnumerator Timer()
+    {
+        timer.text = timeClock.ToString();
+        timeClock -= 1;
+        yield return new WaitForSeconds(1);
+        if (timeClock > 0)
+        {
+            StartCoroutine(Timer());
+        }
+        else
+        {
+            timer.gameObject.SetActive(false);
+            if(Player1 != null)
+            {
+                win1.gameObject.SetActive(true);
+            }
+            else if(Player2 != null)
+            {
+                win2.gameObject.SetActive(true);
+            }
+            else
+            {
+                draw.gameObject.SetActive(true);
+            }
+        }
     }
 }
