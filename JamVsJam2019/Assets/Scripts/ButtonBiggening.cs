c63555fe07667424a24210ba2c8030d6f4895d39
Mint16384 <mint16384@gmail.com>
2019-11-17 15:14:53 -0500

    the bigg works

---
+++ JamVsJam2019/Assets/Scripts/ButtonBiggening.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBiggening : MonoBehaviour
+{
+    Button butt;
+    bool isBig;
+    void Start()
+    {
+        isBig = false;
+        butt = GetComponent<Button>();
+    }
+    void Update()
+    {
+        if(!isBig && UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject == gameObject)
+        {
+            gameObject.transform.localScale *= 1.2f;
+            isBig = true;
+        }
+        if(isBig && UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject != gameObject)
+        {
+            gameObject.transform.localScale /= 1.2f;
+            isBig = false;
+        }
+    }
+}
