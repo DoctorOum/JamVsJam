6928ab95abc1407e376eedd8d680fd580a40d2d5
Sean <43484827+DoctorOum@users.noreply.github.com>
2019-11-17 15:05:18 -0500

    sean wanted me to push this

--- JamVsJam2019/Assets/Scripts/SelectorScript.cs
+++ JamVsJam2019/Assets/Scripts/SelectorScript.cs
@@ -6,62 +6,89 @@ using UnityEngine.SceneManagement;
 public class SelectorScript : MonoBehaviour
 {
     public GameObject RedShip, BlueShip, RedSword, BlueSword;
+    public bool BlueSwordOn, RedSwordOn;
 
     //private Vector2 CharacterPosition, OffScreen;
     private int CharacterInt1 = 1;
     private int CharacterInt2 = 1;
     //private SpriteRenderer RedShipRender, BlueShipRender, RedSwordRender, BlueSwordRender;
 
-    private void Awake()
+    private void Update()
     {
-       //CharacterPosition = RedShip.transform.position;
-       //OffScreen = BlueShip.transform.position;
-       //RedShipRender = RedShip.GetComponent<SpriteRenderer>();
-       //BlueShipRender = BlueShip.GetComponent<SpriteRenderer>();
-       //RedSwordRender = RedSword.GetComponent<SpriteRenderer>();
-       //BlueSwordRender = BlueSword.GetComponent<SpriteRenderer>();
-    }
+        if (Input.GetAxis("J_MainHorizontal1") > 0 && BlueSwordOn == false)
+        {
+            BlueShip.gameObject.SetActive(false);
+            BlueSword.gameObject.SetActive(true);
+            BlueSwordOn = true;
+        }
+        if (Input.GetAxis("J_MainHorizontal1") < 0 && BlueSwordOn == true)
+        {
+            BlueSword.gameObject.SetActive(false);
+            BlueShip.gameObject.SetActive(true);
+            BlueSwordOn = false;
+        }
 
+        if (Input.GetAxis("J_MainHorizontal2") > 0 && RedSwordOn == false)
+        {
+            RedShip.gameObject.SetActive(false);
+            RedSword.gameObject.SetActive(true);
+            RedSwordOn = true;
+        }
+        if (Input.GetAxis("J_MainHorizontal2") < 0 && RedSwordOn == true)
+        {
+            RedSword.gameObject.SetActive(false);
+            RedShip.gameObject.SetActive(true);
+            RedSwordOn = false;
+        }
+    }
+    private void Start()
+    {
+        BlueSwordOn = false;
+        RedSwordOn = false;
+    }
     public void NextCharacter1()
     {
-        switch(CharacterInt1)
-        {
-            case 1:
-                RedShip.gameObject.SetActive(false);
-                RedSword.gameObject.SetActive(true);
-                CharacterInt1++;
-                break;
-            case 2:
-                RedSword.gameObject.SetActive(false);
-                RedShip.gameObject.SetActive(true);
-                CharacterInt1++;
-                ResetInt();
-                break;
-            default:
-                ResetInt();
-                break;
-        }
+            switch (CharacterInt1)
+            {
+
+                case 1:
+                    RedShip.gameObject.SetActive(false);
+                    RedSword.gameObject.SetActive(true);
+                    CharacterInt1++;
+                    break;
+                case 2:
+                    RedSword.gameObject.SetActive(false);
+                    RedShip.gameObject.SetActive(true);
+                    CharacterInt1++;
+                    ResetInt();
+                    break;
+                default:
+                    ResetInt();
+                    break;
+            }
     }
 
     public void PreviousCharacter1()
     {
-
-        switch (CharacterInt1)
+        if (Input.GetAxis("J_MainHorizontal1") < 0)
         {
-            case 1:
-                RedShip.gameObject.SetActive(true);
-                RedSword.gameObject.SetActive(false);
-                CharacterInt1--;
-                ResetInt();
-                break;
-            case 2:
-                RedShip.gameObject.SetActive(false);
-                RedSword.gameObject.SetActive(true);
-                CharacterInt1--;
-                break;
-            default:
-                ResetInt();
-                break;
+            switch (CharacterInt1)
+            {
+                case 1:
+                    RedShip.gameObject.SetActive(true);
+                    RedSword.gameObject.SetActive(false);
+                    CharacterInt1--;
+                    ResetInt();
+                    break;
+                case 2:
+                    RedShip.gameObject.SetActive(false);
+                    RedSword.gameObject.SetActive(true);
+                    CharacterInt1--;
+                    break;
+                default:
+                    ResetInt();
+                    break;
+            }
         }
     }
 
@@ -79,44 +106,49 @@ public class SelectorScript : MonoBehaviour
 
     public void NextCharacter2()
     {
-        switch (CharacterInt2)
+        if (Input.GetAxis("J_MainHorizontal2") > 0)
         {
-            case 1:
-                BlueShip.gameObject.SetActive(false);
-                BlueSword.gameObject.SetActive(true);
-                CharacterInt2++;
-                break;
-            case 2:
-                BlueSword.gameObject.SetActive(false);
-                BlueShip.gameObject.SetActive(true);
-                CharacterInt2++;
-               ResetInt2();
-                break;
-            default:
-                ResetInt2();
-                break;
+            switch (CharacterInt2)
+            {
+                case 1:
+                    BlueShip.gameObject.SetActive(false);
+                    BlueSword.gameObject.SetActive(true);
+                    CharacterInt2++;
+                    break;
+                case 2:
+                    BlueSword.gameObject.SetActive(false);
+                    BlueShip.gameObject.SetActive(true);
+                    CharacterInt2++;
+                    ResetInt2();
+                    break;
+                default:
+                    ResetInt2();
+                    break;
+            }
         }
     }
 
     public void PreviousCharacter2()
     {
-
-        switch (CharacterInt2)
+        if (Input.GetAxis("J_MainHorizontal2") < 0)
         {
-            case 1:
-                BlueShip.gameObject.SetActive(true);
-                BlueSword.gameObject.SetActive(false);
-                CharacterInt2--;
-                ResetInt2();
-                break;
-            case 2:
-                BlueShip.gameObject.SetActive(false);
-                BlueSword.gameObject.SetActive(true);
-                CharacterInt2--;
-                break;
-            default:
-                ResetInt2();
-                break;
+            switch (CharacterInt2)
+            {
+                case 1:
+                    BlueShip.gameObject.SetActive(true);
+                    BlueSword.gameObject.SetActive(false);
+                    CharacterInt2--;
+                    ResetInt2();
+                    break;
+                case 2:
+                    BlueShip.gameObject.SetActive(false);
+                    BlueSword.gameObject.SetActive(true);
+                    CharacterInt2--;
+                    break;
+                default:
+                    ResetInt2();
+                    break;
+            }
         }
     }
 
