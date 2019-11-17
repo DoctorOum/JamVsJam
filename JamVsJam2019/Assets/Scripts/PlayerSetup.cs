774a9abcf8d1b695316edaa42961121d8ad2ab18
Mint16384 <mint16384@gmail.com>
2019-11-17 15:35:50 -0500

    hi

---
+++ JamVsJam2019/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetup : MonoBehaviour
+{
+    GameObject sword, shield;
+
+    void Start()
+    {
+        Invoke("Setup", .25f);
+    }
+    void Setup()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
