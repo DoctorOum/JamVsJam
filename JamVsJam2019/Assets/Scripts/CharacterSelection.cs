b70df0a99aa19a6f96db00a4abb375ce92d60f4b
Sean <43484827+DoctorOum@users.noreply.github.com>
2019-11-16 22:57:52 -0500

    character selection

---
+++ JamVsJam2019/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [Header("UI Reference")]
+    [SerializeField] private Text characterName;
+    [SerializeField] private Image characterSplash;
+    [SerializeField] private Image backgroundColor;
+
+}
